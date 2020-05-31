81d851b497d956594873067e87c4258d0517e12b
Shari Damewood <sharidamewood3@gmail.com>
2020-05-31 02:02:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,14 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else
     true
   end
 end
+=======
+  board[index] != " " || board[index] != "" || board[index] != nil
+  end
+>>>>>>> 8cc90d1c1f0f52ea4ad64c1bb49bedadfc016c3d
